56f156722f9c53effbab07ebd8312db338ea8d92
Nick Liu <imjustanemail@gmail.com>
2016-03-31 14:51:03 +0800

    nothing

---
+++ CarCare/Views/FuelLogCell.swift
@@ -0,0 +1,27 @@
+//
+//  FuelLogCell.swift
+//  CarCare
+//
+//  Created by Nick Liu on 16/3/31.
+//  Copyright © 2016年 Nick Liu. All rights reserved.
+//
+
+import UIKit
+
+class FuelLogCell: UITableViewCell {
+  @IBOutlet weak var distanceLabel: UILabel!
+  @IBOutlet weak var priceLabel: UILabel!
+  @IBOutlet weak var totalPriceLabel: UILabel!
+
+  override func awakeFromNib() {
+      super.awakeFromNib()
+      // Initialization code
+  }
+
+  override func setSelected(selected: Bool, animated: Bool) {
+      super.setSelected(selected, animated: animated)
+
+      // Configure the view for the selected state
+  }
+
+}
